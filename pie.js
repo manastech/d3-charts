1596f198531a42a0569adf2cafaa0c0b8c9afe96
jkicillof <jkicillof@manas.com.ar>
2014-10-10 18:24:19 -0300

    pie transitions

--- pie.js
+++ pie.js
@@ -2,42 +2,85 @@ var width = 960,
     height = 500,
     radius = Math.min(width, height) / 2;
 
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
+var color = d3.scale.category20();
 
 var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d.population; });
+    .value(function(d) { return d.value; })
+    .sort(null);
 
-var svg = d3.select("body").append("svg")
+var arc = d3.svg.arc()
+    .innerRadius(radius - 100)
+    .outerRadius(radius - 20);
+
+var container = d3.select("#chart")
     .attr("width", width)
     .attr("height", height)
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-d3.csv("pie.csv", function(error, data) {
 
-  data.forEach(function(d) {
-    d.population = +d.population;
-  });
+function draw(data) {
 
-  var g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
 
-  g.append("path")
+
+  var exitArc = {startAngle: Math.PI * 2, endAngle: Math.PI * 2};
+  var path = container.datum(data).selectAll("path")
+      .data(pie);
+  if(container.selectAll("path").empty()) {
+    enterPath = path.enter().append("path")
+      .attr("fill", function(d, i) { return color(i); })
       .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.age); });
+      .each(function(d) { this._current = d;});
+  } else {
+    enterPath = path.enter().append("path")
+      .attr("fill", function(d, i) { return color(i); })
+      .attr("d", function(d,i) {return arc(exitArc ,i)})
+      .each(function(d) { this._current = exitArc});
+  }
 
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.age; });
+  path.transition().attrTween("d", arcTween);
+  path.exit().transition().attrTween("d", function(d,i) {return arcTween(exitArc, this)}).remove();
+  lastData = data;
+};
 
+
+document.addEventListener("mouseup", function(e) {
+  draw(randomData());
 });
+
+function randomData() {
+  var data = [];
+  var letter = "ABCDEFGHI".split("");
+  var exponent = Math.round(Math.max(1, Math.random() * 10));
+  d3.range(2, 3 + Math.round(Math.random() * 7)).forEach(function(d) {
+    data.push({key:letter[d - 2], value:Math.round(Math.random() * Math.pow(10, exponent))})
+  });
+  return data;
+}
+
+draw(randomData());
+
+function arcTween(a, path) {
+  path = this._current? this : path;
+  var i = d3.interpolate(path._current, a);
+  path._current = i(0);
+  return function(t) {
+    return arc(i(t));
+  };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
